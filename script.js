524fdc19eb79f552a0e81c08949ed87bd30b183b
yohansbekele12 <yohansdemisie@gmail.com>
2024-08-19 20:59:22 +0200

    "my commite test"

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
- const home=document.querySelector(".home");
 
-home.addEventListener('click' , event=>{
-    event.preventDefault();
-     const audio=new Audio('fuck-sound-effect.mp3');
-    displayCurrectData();
-    
-});
+$("grid-container-activity-detail").show();
 
-function displayCurrectData(){
-  const instantinfo=document.querySelector(".grid-container-activity-detail");
-  const instantinfo2=document.querySelector('.grid-container-runnning-detail');
-  const time=document.getElementById("time");
-  
-  instantinfo.classList.remove('hidden');
-  instantinfo.offsetHeight;
-  instantinfo2.classList.remove('hidden');
-  instantinfo2.offsetHeight;
-  
-  instantinfo.classList.add('show');
-  instantinfo2.classList.add('show');
-  
-}
